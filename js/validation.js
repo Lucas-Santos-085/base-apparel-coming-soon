e711b0d18a985d52df26b43f57a7167d9ec9472d
Lucas Santos <lucas.santos085@gmail.com>
2021-06-13 00:23:12 -0300

    hover

--- js/validation.js
+++ js/validation.js
@@ -1,40 +1,24 @@
 
-    var arrowButton = document.querySelector(".submit");
+    const arrowButton = document.querySelector(".submit");
+    const divError = document.querySelector(".alert-error");
 
     arrowButton.addEventListener("click", function(event) {
     
     event.preventDefault();
+    divError.innerHTML = '';
 
     const inputEmail = document.querySelector(".input");
     const errorEmail = document.querySelector(".error-message");
 
     if(inputEmail.validity.valid) {
         errorEmail.innerHTML = '';
-        console.log("email válido")
     } else {
-        console.log("email inválido")
         errorEmail.innerHTML = 'Please provide a valid email';
-        insertErrorIcon2();
+        insertErrorIcon();
     }
     });
 
-
-    function insertErrorIcon(){
-        
-        const divError = document.querySelector(".alert-error");
-
-        const iconError = document.createElement('span');
-
-        iconError.classList.add('error-icon');
-
-        divError.appendChild(iconError);
-
-
-    }
-
-    function insertErrorIcon2 (){
-
-        const divError = document.querySelector(".alert-error");
+    function insertErrorIcon (){
         const img = document.createElement("img");
         img.setAttribute('src', 'js/icon-error.svg');
         divError.appendChild(img);
